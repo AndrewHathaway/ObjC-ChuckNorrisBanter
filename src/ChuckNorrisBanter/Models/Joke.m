8596ef12953d319d7db36e6491169e7f1dc1b5cb
James Billingham <james@billingham.net>
2013-10-02 15:56:15 -0700

    silleh andiroo

---
+++ src/ChuckNorrisBanter/Models/Joke.m
@@ -0,0 +1,13 @@
+//
+//  Joke.m
+//  ChuckNorrisBanter
+//
+//  Created by James Billingham on 10/02/13.
+//  Copyright (c) 2013 Andrew Hathaway. All rights reserved.
+//
+
+#import "Joke.h"
+
+@implementation Joke
+
+@end
---
+++ src/ChuckNorrisBanter/Models/JokeResponse.m
@@ -0,0 +1,13 @@
+//
+//  JokeResponse.m
+//  ChuckNorrisBanter
+//
+//  Created by James Billingham on 10/02/13.
+//  Copyright (c) 2013 Andrew Hathaway. All rights reserved.
+//
+
+#import "JokeResponse.h"
+
+@implementation JokeResponse
+
+@end
